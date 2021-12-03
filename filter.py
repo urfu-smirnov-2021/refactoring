8754c2cfded6318bae654bbecf983b457fc12ef9
Matthew <sarapulov.03@mail.ru>
2021-12-03 22:03:51 +0500

    First task

--- filter.py
+++ filter.py
@@ -1,28 +1,35 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 11:
-    j = 0
-    while j < a1 - 11:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+
+
+def generate_img():
+    img = Image.open("img2.jpg")
+    arr = np.array(img)
+    a = len(arr)
+    a1 = len(arr[1])
+    i = 0
+    while i < a - 9:
+        j = 0
+        while j < a1 - 9:
+            s = 0
+            for n in range(i, i + 10):
+                for n1 in range(j, j + 10):
+                    c1 = arr[n][n1][0]
+                    c2 = arr[n][n1][1]
+                    c3 = arr[n][n1][2]
+                    M = int(c1) + int(c2) + int(c3)
+                    s += M
+            s = int(s // 100)
+            for n in range(i, i + 10):
+                for n1 in range(j, j + 10):
+                    arr[n][n1][0] = int(s // 150) * 50
+                    arr[n][n1][1] = int(s // 150) * 50
+                    arr[n][n1][2] = int(s // 150) * 50
+            j = j + 10
+        i = i + 10
+    res = Image.fromarray(arr)
+    res.save('res.jpg')
+
+
+if __name__ == "__main__":
+    generate_img()
