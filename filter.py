be1c62a83132527788c2d9da3f7099bdd17fceaf
Kremix1 <80645579+Kremix1@users.noreply.github.com>
2021-12-03 18:35:06 +0500

    Last stage (Немного намудрил, но вроде работает)

--- filter.py
+++ filter.py
@@ -1,14 +1,12 @@
 from PIL import Image
 import numpy as np
-input_image = Image.open("img2.jpg")
-image_array = np.array(input_image)
 
-def get_average_color(image_array, width, mosaic_width, height, mosaic_height):
-    mosaic_resolution = mosaic_height * mosaic_width
-    average_color_on_screen = np.sum(image_array[width: width + mosaic_width, height: height + mosaic_height]) //  3
-    average_color = average_color_on_screen // mosaic_resolution
-    return average_color
-
+def transform_image_to_mosaic(input_image, mosaic_width, mosaic_height, scale, mosaic_name, format_name):
+    image_array = np.array(input_image)
+    mosaic_array = get_gray_mosaic_array(image_array, mosaic_width, mosaic_height, scale)
+    mosaic_image = Image.fromarray(mosaic_array)
+    mosaic_image.save(mosaic_name + format_name)
+    
 def get_gray_mosaic_array(image_array, mosaic_width, mosaic_height, scale):
     width = len(image_array)
     height = len(image_array[1])
@@ -19,9 +17,16 @@ def get_gray_mosaic_array(image_array, mosaic_width, mosaic_height, scale):
             image_array[current_width: current_width + mosaic_width, current_height: current_height + mosaic_height] = np.full(3, gray_color)
     return image_array
 
+def get_average_color(image_array, width, mosaic_width, height, mosaic_height):
+    mosaic_resolution = mosaic_height * mosaic_width
+    average_color_on_screen = np.sum(image_array[width: width + mosaic_width, height: height + mosaic_height]) //  3
+    average_color = average_color_on_screen // mosaic_resolution
+    return average_color
+
+input_image = Image.open(input("Путь до изображения -  "))
 mosaic_width = int(input("Целое положительное число для ширины мозайки - "))
 mosaic_height = int(input("Целое положительное число для высоты мозайки - "))
 scale = int(input("Целое положительное число для масштаба изменения серых цветов в мозайке - "))
-mosaic_array = get_gray_mosaic_array(image_array, mosaic_width, mosaic_height, scale)
-mosaic_image = Image.fromarray(mosaic_array)
-mosaic_image.save('res.jpg')
+mosaic_name = input("Введите название для готовой мозайки: ")
+format_name = input("Введите формат для готовой мозайки: ")
+transform_image_to_mosaic(input_image, mosaic_width, mosaic_height, scale, mosaic_name, format_name)
