f9681a6467f234d3ed3009f743385d3341197ac5
Kremix1 <80645579+Kremix1@users.noreply.github.com>
2021-12-03 18:18:32 +0500

    First-lap

--- filter.py
+++ filter.py
@@ -1,28 +1,28 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 11:
-    j = 0
-    while j < a1 - 11:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+input_image = Image.open("img2.jpg")
+image_array = np.array(input_image)
+width = len(image_array)
+height = len(image_array[1])
+current_width = 0
+while current_width < width - 9:
+    current_height = 0
+    while current_height < height - 9:
+        total_brightness = 0
+        for n in range(current_width, current_width + 10):
+            for k in range(current_height, current_height + 10):
+                first_component = image_array[n][k][0]
+                second_component = image_array[n][k][1]
+                third_component = image_array[n][k][2]
+                local_brightness = int(first_component) + int(second_component) + int(third_component)
+                total_brightness += local_brightness
+        total_brightness = int(total_brightness / 3 // 100)
+        for n in range(current_width, current_width + 10):
+            for comp in range(current_height, current_height + 10):
+                image_array[n][comp][0] = int(total_brightness // 50) * 50
+                image_array[n][comp][1] = int(total_brightness // 50) * 50
+                image_array[n][comp][2] = int(total_brightness // 50) * 50
+        current_height = current_height + 10
+    current_width = current_width + 10
+mosaic_image = Image.fromarray(image_array)
+mosaic_image.save('res.jpg')
